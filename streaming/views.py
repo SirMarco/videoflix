fcc39be4863df70bcd0f5c28d7e971ff93017584
Marco Engelhardt <marco.engelhardt@reussnet.ch>
2024-09-25 16:37:11 +0200

    transfer

--- streaming/views.py
+++ streaming/views.py
@@ -6,6 +6,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+
 from django.utils.http import urlsafe_base64_decode
 from django.utils.encoding import force_str
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
@@ -69,4 +71,44 @@ class ActivateView(APIView):
             return Response({'message': 'Account erfolgreich aktiviert'}, status=status.HTTP_200_OK)
         else:
             return Response({'error': 'Ungültiger oder abgelaufener Link'}, status=status.HTTP_400_BAD_REQUEST)
+        
+class PasswordResetRequestView(APIView):
+    def post(self, request, *args, **kwargs):
+        email = request.data.get('email')
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            return Response({'error': 'E-Mail ist nicht registriert.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        token_generator = PasswordResetTokenGenerator()
+
+        reset_link = f"http://localhost:4200/password-reset/{user.pk}/{token_generator.make_token(user)}"
+        mail_subject = 'Passwort zurücksetzen'
+        message = render_to_string('password_reset_email.html', {
+            'user': user,
+            'reset_link': reset_link,
+        })
+        send_mail(mail_subject, message, settings.DEFAULT_FROM_EMAIL, [email])
+
+        return Response({'message': 'Überprüfe deine E-Mail, um dein Passwort zurückzusetzen.'}, status=status.HTTP_200_OK)     
     
+class PasswordResetConfirmView(APIView):
+    def post(self, request, *args, **kwargs):
+        uidb64 = request.data.get('uid')
+        token = request.data.get('token')
+        new_password = request.data.get('new_password')
+
+        try:
+            # uid = force_str(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(user.pk)
+        except (User.DoesNotExist, ValueError, TypeError, OverflowError):
+            return Response({'error': 'Ungültiger Benutzer.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        token_generator = PasswordResetTokenGenerator()
+
+        if user is not None and token_generator.check_token(user, token):
+            user.set_password(new_password)
+            user.save()
+            return Response({'message': 'Passwort erfolgreich zurückgesetzt.'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Ungültiges Token oder abgelaufener Link.'}, status=status.HTTP_400_BAD_REQUEST)
--- videoflix/urls.py
+++ videoflix/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 
-from streaming.views import LoginView, RegisterView, ActivateView
+from streaming.views import LoginView, PasswordResetConfirmView, PasswordResetRequestView, RegisterView, ActivateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/login/', LoginView.as_view(), name='login'),
     path('api/v1/register/', RegisterView.as_view(), name='register'),
     path('api/v1/activate/<int:id>/<str:token>/', ActivateView.as_view(), name='activate'),
+    path('password-reset/', PasswordResetRequestView.as_view(), name='password_reset'),
+    path('password-reset/confirm/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 ]
