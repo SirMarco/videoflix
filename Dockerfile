ffd37262c6710ed1c69ae1d9c4dd7bee8e63cc0a
Marco Engelhardt <marco.engelhardt@reussnet.ch>
2024-10-11 16:43:09 +0200

    transfer

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 # COPY ./backend .
 
-CMD ["python3", "manage.py", "runserver", "0.0.0.0:8000"]
+# CMD ["python3", "manage.py", "runserver", "0.0.0.0:8000"]
--- backend/videoflix/settings.py
+++ backend/videoflix/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l3#!b$kqxc9ocdia0)vko7q5*sz&06vn)&)tcuih&a)nc=@dxp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
