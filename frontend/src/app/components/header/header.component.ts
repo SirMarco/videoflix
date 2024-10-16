ca02310ef25f0056aca4037d9afe6f8b9a769495
Marco Engelhardt <sirmarco@reussnet.ch>
2024-10-16 21:12:08 +0200

    remove viceoplay, add imprint

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DeviceService } from '../../services/device.service';
-import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth.service';
 
@@ -13,8 +13,9 @@ import { AuthService } from '../../services/auth.service';
 })
 export class HeaderComponent implements OnInit {
   isDesktop: boolean = false;
-  showLoginButton: boolean = true;
+  showBackButton: boolean = false;
   isLoggedIn: boolean = false;
+
 
   constructor(
     private deviceService: DeviceService,
@@ -22,19 +23,16 @@ export class HeaderComponent implements OnInit {
     private activatedRoute: ActivatedRoute,
     private authService: AuthService
   ) {
-    this.router.events.subscribe(() => {
-      let currentRoute = this.router.url;
-      if (currentRoute === '/imprint' || currentRoute === '/privacy') {
-        this.showLoginButton = false;
-      } else {
-        this.showLoginButton = true;
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const currentRoute = this.router.url;
+        this.showBackButton = currentRoute === '/imprint' || currentRoute === '/privacy';
       }
     });
   }
 
   ngOnInit() {
     this.isDesktop = this.deviceService.isDesktop;
-
     this.authService.isLoggedIn().subscribe((status) => {
       this.isLoggedIn = status;
     });
@@ -42,6 +40,7 @@ export class HeaderComponent implements OnInit {
 
   logout() {
     this.authService.logout();
+    this.router.navigate(['/login']);
   }
 
   goBack() {
--- frontend/src/app/components/video-detail/video-detail.component.ts
+++ frontend/src/app/components/video-detail/video-detail.component.ts
@@ -1,99 +1,32 @@
-import {
-  AfterViewInit,
-  CUSTOM_ELEMENTS_SCHEMA,
-  Component,
-  OnInit,
-  ViewChild,
-} from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { VideoPlayerComponent } from '../video-player/video-player.component';
 import { Video } from '../../interfaces/video.interface';
 import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { NgxSpinnerService } from 'ngx-spinner';
-import { NgxSpinnerModule } from 'ngx-spinner';
-import { VgCoreModule } from '@videogular/ngx-videogular/core';
-import { VgControlsModule } from '@videogular/ngx-videogular/controls';
-import { VgOverlayPlayModule } from '@videogular/ngx-videogular/overlay-play';
-import { VgBufferingModule } from '@videogular/ngx-videogular/buffering';
-// DEFAULT LAYOUT
-// import 'vidstack/player/styles/default/theme.css';
-// import 'vidstack/player/styles/default/layouts/video.css';
-// import 'vidstack/player';
-// import 'vidstack/player/layouts/default';
-// import 'vidstack/player/ui';
-// DEFAULT LAYOUT
-
-// plyr THEME
-import 'vidstack/player/styles/base.css';
-import 'vidstack/player/styles/plyr/theme.css';
-import 'vidstack/player';
-import 'vidstack/player/layouts/plyr';
-import 'vidstack/player/ui';
-import Plyr from 'plyr';
-
-import Hls from 'hls.js';
-// plyr THEME
+import { NgxSpinnerModule } from "ngx-spinner";
 
 @Component({
   selector: 'app-video-detail',
   standalone: true,
-  imports: [
-    VideoPlayerComponent,
-    CommonModule,
-    NgxSpinnerModule,
-    VgCoreModule,
-    VgControlsModule,
-    VgOverlayPlayModule,
-    VgBufferingModule,
-  ],
+  imports: [CommonModule, NgxSpinnerModule],
   templateUrl: './video-detail.component.html',
   styleUrls: ['./video-detail.component.scss'],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class VideoDetailComponent implements OnInit, AfterViewInit {
+export class VideoDetailComponent implements OnInit {
   videoId: string | null = null;
   videoData?: Video;
   url = environment.baseUrl + '/videos/';
   hlsUrl = environment.hlsUrl;
   pictureUrl = environment.pictureUrl;
 
-  public events: string[] = [];
-
-  @ViewChild(VideoPlayerComponent) videoPlayer!: VideoPlayerComponent;
-  // player: any;
-
-  videoSrc =
-    'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4';
-
-  constructor(
-    private route: ActivatedRoute,
-    private http: HttpClient,
-    private spinner: NgxSpinnerService
-  ) {}
+  constructor(private route: ActivatedRoute, private http: HttpClient, private spinner: NgxSpinnerService) { }
 
   ngOnInit(): void {
     this.videoId = this.route.snapshot.paramMap.get('id');
     this.getSingleVideo();
-  }
-
-  ngAfterViewInit() {
-    const player = new Plyr('#player', { fullscreen: { enabled: true } });
-  }
-
-  initializePlayer() {
-    const mediaPlayer = document.querySelector('media-player');
-
-    if (mediaPlayer) {
-      const storageKey = mediaPlayer.getAttribute('storage-key');
-      console.log('Storage Key:', storageKey);
-
-      // Du kannst auch andere Events hier abfangen
-      mediaPlayer.addEventListener('loadedmetadata', () => {
-        console.log('Metadata geladen');
-      });
-    }
   }
 
   getSingleVideo() {
@@ -108,8 +41,5 @@ export class VideoDetailComponent implements OnInit, AfterViewInit {
         console.error('Fehler beim Laden des Videos:', error);
       }
     );
-  }
-  public eventFired(event: string): void {
-    this.events.push(event);
   }
 }
