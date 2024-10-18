f537b26a6abd743de22ec3844e00c9be21439634
Marco Engelhardt <sirmarco@reussnet.ch>
2024-10-18 10:26:21 +0200

    fixes

--- backend/videoflix/settings.py
+++ backend/videoflix/settings.py
@@ -69,15 +69,15 @@ MIDDLEWARE = [
 CSRF_TRUSTED_ORIGINS = [
   'http://127.0.0.1',
   'http://localhost',
+  'https://videoflix.marco-engelhardt.ch'
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-
-
 CORS_ALLOWED_ORIGINS = [
   'http://127.0.0.1',
   'http://localhost',
+  'https://videoflix.marco-engelhardt.ch'
 ]
 
 INTERNAL_IPS = [
