ace70419c89dd1dc947a756e755e03d57a0b8627
Marco Engelhardt <sirmarco@reussnet.ch>
2024-10-21 22:28:35 +0200

    fixes websocket

--- backend/streaming/routing.py
+++ backend/streaming/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'wss/conversion-status/', consumers.ConversionStatusConsumer.as_asgi()),
+    re_path(r'ws/conversion-status/', consumers.ConversionStatusConsumer.as_asgi()),
 ]
