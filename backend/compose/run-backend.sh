441fa4bdbc7cdeb7ddce3d04eb1ac50a94d5d220
Marco Engelhardt <sirmarco@reussnet.ch>
2024-11-10 16:21:46 +0100

    sfsdfsdf

--- backend/compose/run-backend.sh
+++ backend/compose/run-backend.sh
@@ -3,4 +3,3 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
 gunicorn --workers=1 --bind=0.0.0.0:8000 videoflix.wsgi:application
-# daphne -b 0.0.0.0 -p 8001 videoflix.asgi:application
--- backend/start_service.sh
+++ backend/start_service.sh
@@ -3,4 +3,3 @@ python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 gunicorn --workers=3 --bind=0.0.0.0:8000 videoflix.wsgi:application
-# daphne -b 0.0.0.0 -p 8001 videoflix.asgi:application
